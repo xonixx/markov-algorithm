b50d70bdad88eb8166ca3ab5fc03c9743b4f8f7e
xonix <xonixx@gmail.com>
2022-05-20 15:37:50 +0300

    restore python2 version of markov.py

--- markov.py
+++ markov.py
@@ -3,12 +3,12 @@ DELIM_SYMBOL = '>'
 
 import sys
 if len(sys.argv) > 1:
-	FILE_NAME = sys.argv[1]
+    FILE_NAME = sys.argv[1]
 else:
-	FILE_NAME = 'alg.txt'
+    FILE_NAME = 'alg.txt'
 
 arr=[s for s in open(FILE_NAME, 'r').read().split('\n') \
-	 if s.lstrip() and s.lstrip()[0] != COMMENT_SYMBOL ]
+     if s.lstrip() and s.lstrip()[0] != COMMENT_SYMBOL ]
 
 DATA_STRING = arr[0]
 
@@ -20,43 +20,47 @@ ALG_PAIRS=[ tuple(s.split(DELIM_SYMBOL)) for s in ALG ]
 _s = DATA_STRING
 
 class Exit:
-	pass
+    pass
 
 while True:
-	try:
-		for i in range( len( ALG_PAIRS ) ):
-			_rule_applied = False
+    try:
+        for i in range( len( ALG_PAIRS ) ):
+            _rule_applied = False
 
-			pair = ALG_PAIRS[i]
+            pair = ALG_PAIRS[i]
 
-			if pair[0] in _s:
-				_rule_applied = True
+            _from = pair[0]
+            if _from in _s:
+                _rule_applied = True
 
-				_repl = pair[1]
-				_term = False
+                _repl = pair[1]
+                _term = False
 
-				if _repl[-1]=='.': # this is terminating rule
-					_term = True
-					_repl = _repl[0:-1]
+                if len(_repl) and _repl[-1]=='.': # this is terminating rule
+                    _term = True
+                    _repl = _repl[0:-1]
 
-				print '    Applying rule', i, ALG[i]
-				print _s,'->',
+                print '    Applying rule', i, ALG[i]
+                print _s,'->',
 
-				_s = _s.replace(pair[0], _repl, 1)
+                if (_s == '' and _from == ''):
+                    _s = _repl
+                else:
+                    _s = _s.replace(_from, _repl, 1)
 
-				print _s+'\n'
+                print _s+'\n'
 
-				if _term:
-					print '   Terminating rule!'
-					raise Exit()
+                if _term:
+                    print '   Terminating rule!'
+                    raise Exit()
 
-				break
+                break
 
-		if not _rule_applied:
-			print '    No rules matched!'
-			break # no one rules matched
+        if not _rule_applied:
+            print '    No rules matched!'
+            break # no one rules matched
 
-	except Exit:
-		break
+    except Exit:
+        break
 
 print 'Result : '+_s
